36a879c733f2f8e5bba7cb5a41ca3fe81e74ab38
Oliver Nygren <oliver.nygren@icloud.com>
2020-12-02 20:46:17 +0100

    small changes

--- script.js
+++ script.js
@@ -14,24 +14,15 @@ window.onscroll = function() {
 const desktopNav = document.querySelector('.desktop-nav');
 const menuIcon = document.querySelector('.hamburger-menu');
 const mobileNav = document.querySelector('.mobile-nav');
-const windowWidth = window.innerWidth;
+// const mobileNavLink = document.querySelectorAll('.mobile-nav-link');
 
 menuIcon.addEventListener('click', () => {
     mobileNav.classList.toggle('change');
-
-    // if (desktopNav.classList.contains('display-none')) {
-
-    //     desktopNav.classList.remove('display-none');
-    //     mobileNav.classList.add('display-none');
-    
-    // } else {
-    
-    //     desktopNav.classList.add('display-none');
-    //     mobileNav.classList.remove('display-none');
-    
-    // }
-
 });
+
+// mobileNavLink.addEventListener('click', () => {
+//     mobileNav.classList.toggle('change');
+// });
 
 
 
